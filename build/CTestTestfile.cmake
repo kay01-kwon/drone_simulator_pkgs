0532dba034bde5adf3845eaa565e7da36dc39ac4
kay <kay1216@yonsei.ac.kr>
2026-05-13 13:56:54 +0900

    data added

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/kay/rotors_sim_ws/src/drone_simulator_pkgs/drone_bringup
+# Build directory: /home/kay/rotors_sim_ws/src/drone_simulator_pkgs/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
